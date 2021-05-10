c141a67f1a7d7b4093abcdf2dcc29e905f621641
edss1 <edss1@naver.com>
2021-05-10 09:07:48 +0900

    0510home

--- KingdomMMO/Assets/Scripts/Contents/PlayerStatus.cs
+++ KingdomMMO/Assets/Scripts/Contents/PlayerStatus.cs
@@ -4,8 +4,8 @@
 스크립트 설명 : 플레이어 전용 스테이터스 스크립트
 스크립트 사용법 : 
                  
-수정일자(1차) :                                       
-수정내용(1차) :                                   
+수정일자(1차) : 2021-05-07
+수정내용(1차) : 획득률 증가 헤더 및 세부 변수 추가, 공격력을 원거리공격력, 근거리공격력으로 세분화
                                   
 */
 
@@ -20,7 +20,7 @@ public class PlayerStatus : Status
     [SerializeField]
     private int strength;
     [SerializeField]
-    private int dextility;
+    private int dexterity;
     [SerializeField]
     private int agility;
     [SerializeField]
@@ -28,10 +28,16 @@ public class PlayerStatus : Status
     [SerializeField]
     private int intelligence;
     [SerializeField]
-    private int wisdom;
+    private int energe;
     [SerializeField]
     private int lucky;
 
+    //근거리, 원거리 공격력
+    [Header("플레이어 스테이터스")]
+    [SerializeField]
+    private int swordAtk;
+    [SerializeField]
+    private int rangeAtk;
 
     //회복량
     [Header("플레이어 자연회복량")]
@@ -66,13 +72,26 @@ public class PlayerStatus : Status
     [SerializeField]
     private int diamond;
 
+    //획득량 및 획득률 증가
+    [Header("획득률 증가")]
+    [SerializeField]
+    private float gainingRateExp;
+    [SerializeField]
+    private float gainingGold;
+    [SerializeField]
+    private float gainingNomalMeterial;
+    [SerializeField]
+    private float gainingEquipmentAndRarityMeterial;
+
     public int Str { get { return strength; } set { strength = value; } }
-    public int Dex { get { return dextility; } set { dextility = value; } }
+    public int Dex { get { return dexterity; } set { dexterity = value; } }
     public int Agi { get { return agility; } set { agility = value; } }
     public int Vit { get { return vitality; } set { vitality = value; } }
     public int Int { get { return intelligence; } set { intelligence = value; } }
-    public int Wis { get { return wisdom; } set { wisdom = value; } }
+    public int Eng { get { return energe; } set { energe = value; } }
     public int Luk { get { return lucky; } set { lucky = value; } }
+    public int SwordAtk { get { return swordAtk; } set { swordAtk = value; } }
+    public int RangeAtk { get { return rangeAtk; } set { rangeAtk = value; } }
     public int HpRegen { get { return hpRegen; } set { hpRegen = value; } }
     public int MpRegen { get { return mpRegen; } set { mpRegen = value; } }
     public int Weight { get { return weight; } set { weight = value; } }
@@ -82,6 +101,12 @@ public class PlayerStatus : Status
     public int MaxExp { get { return maxExp; } set { maxExp = value; } }
     public int Gold { get { return gold; } set { gold = value; } }
     public int Diamond { get { return diamond; } set { diamond = value; } }
+    public float GainingRateExp { get { return gainingRateExp; } set { gainingRateExp = value; } }
+    public float GainingGold { get { return gainingGold; } set { gainingGold = value; } }
+    public float GainingNomalMeterial { get { return gainingNomalMeterial; } set { gainingNomalMeterial = value; } }
+    public float GainingEquipmentAndRarityMeterial { get { return gainingEquipmentAndRarityMeterial; } set { gainingEquipmentAndRarityMeterial = value; } }
+
+
 
     // Start is called before the first frame update
     void Start()
