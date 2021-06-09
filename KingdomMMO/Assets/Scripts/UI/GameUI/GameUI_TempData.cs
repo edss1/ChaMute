f03b5020a89520e3e629aa59e519846dc59b1eb8
ChaMute <edss1@naver.com>
2021-06-09 12:28:13 +0900

    0609SGA

---
+++ KingdomMMO/Assets/Scripts/UI/GameUI/GameUI_TempData.cs
@@ -0,0 +1,51 @@
+﻿//GameUI_TempData 스크립트
+/*
+ * 작성일자 : 2021-06-08
+스크립트 설명 : 게임중 획득한 아이템을 출력해주는 스크립트
+스크립트 사용법 : 
+                 
+수정일자(1차) :                                       
+수정내용(1차) :                                   
+                                  
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI_TempData : MonoBehaviour
+{
+    [SerializeField]
+    Button tempDataPopupButton;
+    [SerializeField]
+    Button tempDataExitButton;
+    [SerializeField]
+    Image tempDataImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        tempDataImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempDataPopupButton.onClick.AddListener(PopupButton);
+        tempDataExitButton.onClick.AddListener(ExitButton);
+    }
+
+    //일시정지 활성화
+    void PopupButton()
+    {
+        tempDataImage.gameObject.SetActive(true);
+    }
+
+    void ExitButton()
+    {
+        tempDataImage.gameObject.SetActive(false);
+
+    }
+
+}
